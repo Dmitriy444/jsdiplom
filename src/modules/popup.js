a8f2e8307301969ebfe6c9089ad10ae26a5e9c55
Dmitri Larin <Diman-Larin@mail.ru>
2021-03-23 23:33:54 +0400

    popup-close

--- src/modules/popup.js
+++ src/modules/popup.js
@@ -1,15 +1,37 @@
 const popup = () =>{
     const btnCall = (document.querySelectorAll('div.relative a')[1]),
         modalCallback = document.getElementById('callback'),
-        overlay = document.querySelector('.modal-overlay');
+        overlay = document.querySelector('.modal-overlay'),
+        body = document.querySelector('body'),
+        modalClose = document.querySelector('div.modal-close img');
 
     modalCallback.style.display = 'none';
     overlay.style.display = 'none';
 
-    btnCall.addEventListener('click', () => {
-        modalCallback.style.display = modalCallback.style.display === 'none' ? 'block' : 'none';
-        overlay.style.display = overlay.style.display === 'none' ? 'block' : 'none';
+    modalClose.classList.add('popupCloseBtn');
+    btnCall.classList.add('btnOpen');
+
+    body.addEventListener('click', (event) => {
+        let target = event.target;
+        if(target.classList.contains('btnOpen')){
+            modalCallback.style.display ='block';
+            overlay.style.display = 'block';
+        } else if(target.classList.contains('popupCloseBtn')){
+            modalCallback.style.display ='none';
+            overlay.style.display = 'none';
+        }
     });
+
+    overlay.addEventListener('click', (event) => {
+        let target = event.target;
+            target = target.closest('#callback');
+            if(!target){
+                modalCallback.style.display ='none';
+                overlay.style.display = 'none';
+            }
+        });
+
+
 };
 
 
